0d3713af35e63bb15db2d715d8146d471b800715
mateobaudracco22 <mateobaudracco@gmail.com>
2026-09-18 16:38:32 -0300

    creando página de informacion
    Es una sección dedicada a brindar más información sobre este gimnasio, para que los usuarios vean todo el contenido de nuestra página

---
+++ src/paginas/informacion.jsx
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Información - Impacto Fitness</title>
+</head>
+<body>
+    <header>
+        <h1>Información sobre este gimnasio</h1>
+        <h2>Impacto-Fitness</h2>
+    </header>
+
+        <main class="contenedor">
+          <div class="caja">
+            <h2>¿Quiénes somos?</h2>
+            <p>
+                En Impacto-Fitness buscamos facilitar la tarea de entrenar
+                y ayudar a nuestros usuarios a organizar sus actividades
+                de una manera sencilla y rápida.
+            </p>
+          </div>
+          <div class="caja">
+
+          </div>
+        </main>
+</body>
+</html>
