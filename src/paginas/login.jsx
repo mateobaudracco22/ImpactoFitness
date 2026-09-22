8c616641edbb83cedc95eba2ee74b858182a24d7
Sebastián Illanes <sebastianillanesroojass@gmail.com>
2026-09-22 20:30:35 -0300

    Placeholders agregados

--- src/paginas/login.jsx
+++ src/paginas/login.jsx
@@ -6,11 +6,11 @@ function login() {
             <h1>Iniciar Sesión</h1>
             <form>
                 <label>Nombre y Apellido</label>
-                <input type="text"/>
+                <input type="text" placeholder="Ingresa tu nombre" />
                 <label>Usuario</label>
-                <input type="text" />
-            <label>Email</label>
-                <input type="email" />
+                <input type="text" placeholder="Ingresa tu usuario" />
+                <label>Email</label>
+                <input type="email" placeholder="correo@ejemplo.com" />
                 <label>Contraseña</label>
                 <input type="password" />
                 <button type="submit">Ingresar</button>
