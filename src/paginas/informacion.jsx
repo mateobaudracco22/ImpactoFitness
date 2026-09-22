b1fb7e619943b7ef37c3b13818f01dad936bc35a
mateobaudracco22 <mateobaudracco@gmail.com>
2026-09-22 20:36:52 -0300

    nuevos cambios en información

--- src/paginas/informacion.jsx
+++ src/paginas/informacion.jsx
@@ -3,7 +3,7 @@ import "../estilos/informacion.css";
 
 function Info() {
     return (
-        <div>
+        <div className="info">
 
             <header>
                 <h1>Información sobre este gimnasio</h1>
@@ -12,7 +12,7 @@ function Info() {
 
             <main className="contenedor">
 
-                <div className="info">
+                <div className="caja">
                     <h2>¿Quiénes somos?</h2>
                     <p>
                         En Impacto-Fitness buscamos facilitar la tarea de entrenar
@@ -21,7 +21,7 @@ function Info() {
                     </p>
                 </div>
 
-                <div className="info">
+                <div className="caja">
                     <h2>¿Qué ofrecemos?</h2>
                     <p>
                         Nuestra plataforma permite acceder a diferentes servicios
@@ -36,7 +36,7 @@ function Info() {
                     </ul>
                 </div>
 
-                <div className="info">
+                <div className="caja">
                     <h2>Rutinas</h2>
                     <p>
                         Los usuarios pueden consultar diferentes rutinas dependiendo
@@ -45,7 +45,7 @@ function Info() {
                     </p>
                 </div>
 
-                <div className="info">
+                <div className="caja">
                     <h2>Reservas</h2>
                     <p>
                         Desde la plataforma se pueden realizar reservas para
@@ -54,7 +54,7 @@ function Info() {
                     </p>
                 </div>
 
-                <div className="info">
+                <div className="caja">
                     <h2>Horarios</h2>
                     <p>
                         El gimnasio cuenta con diferentes horarios para que cada
@@ -63,7 +63,7 @@ function Info() {
                     </p>
                 </div>
 
-                <div className="info">
+                <div className="caja">
                     <h2>Contacto</h2>
                     <p>
                         Para realizar consultas o recibir más información,
