bca2638ad53c83a17304d1cd3b4a2b937053bf93
Sebastián Illanes <sebastianillanesroojass@gmail.com>
2026-09-22 20:41:24 -0300

    Estructur de login.jsx pulida

--- src/paginas/login.jsx
+++ src/paginas/login.jsx
@@ -6,23 +6,28 @@ function login() {
      <div className="tarjeta-login">
       <h1 className="titulo-login">Iniciar Sesión</h1>
        <form className="formulario-login">
-                <div className="campo">
-                <label>Nombre y Apellido</label>
-                <input type="text" placeholder="Ingresa tu nombre" />
-                </div>
-                <div className="campo">
-                <label>Usuario</label>
-                <input type="text" placeholder="Ingresa tu usuario" />
-                </div>
-                <div className="campo">
-                <label>Email</label>
-                <input type="email" placeholder="correo@ejemplo.com" />
-                </div>
-                <div className="campo">
-                <label>Contraseña</label>
-                <input type="password" />
-                </div>
-                <button type="submit">Ingresar</button>
+        
+        <div className="campo">
+         <label>Nombre y Apellido</label>
+         <input type="text" placeholder="Ingresa tu nombre" />
+        </div>
+
+        <div className="campo">
+         <label>Usuario</label>
+         <input type="text" placeholder="Ingresa tu usuario" />
+        </div>
+
+        <div className="campo">
+         <label>Email</label>
+         <input type="email" placeholder="correo@ejemplo.com" />
+        </div>
+
+        <div className="campo">
+         <label>Contraseña</label>
+         <input type="password" />
+        </div>
+
+        <button type="submit">Ingresar</button>
        </form>
      </div>
     </div>
