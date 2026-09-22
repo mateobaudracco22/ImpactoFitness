d68709cba793cf97e19790f8744e8f6198c5b89d
Sebastián Illanes <sebastianillanesroojass@gmail.com>
2026-09-22 20:36:38 -0300

    Clases y campos agregadas

--- src/paginas/login.jsx
+++ src/paginas/login.jsx
@@ -2,20 +2,30 @@ Import React from "react";
 
 function login() {
     return (
-        <div>
-            <h1>Iniciar Sesión</h1>
-            <form>
+    <div className="contenedor-login">
+     <div className="tarjeta-login">
+      <h1 className="titulo-login">Iniciar Sesión</h1>
+       <form className="formulario-login">
+                <div className="campo">
                 <label>Nombre y Apellido</label>
                 <input type="text" placeholder="Ingresa tu nombre" />
+                </div>
+                <div className="campo">
                 <label>Usuario</label>
                 <input type="text" placeholder="Ingresa tu usuario" />
+                </div>
+                <div className="campo">
                 <label>Email</label>
                 <input type="email" placeholder="correo@ejemplo.com" />
+                </div>
+                <div className="campo">
                 <label>Contraseña</label>
                 <input type="password" />
+                </div>
                 <button type="submit">Ingresar</button>
-            </form>
-        </div>
+       </form>
+     </div>
+    </div>
     );
 }
 
